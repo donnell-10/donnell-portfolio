d9256650befba6b9f1420eaadfcd9fc0ad2ac57a
dxo024 <dxo024@cs.bham.ac.uk>
2025-07-15 22:10:00 +0100

    adding contact buttons and picture carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,53 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import ContactButtons from './components/ContactButtons'
+import "./App.css";
+import ContactButtons from "./components/ContactButtons";
+import HomeCarousel from "./components/HomeCarousel";
 //import MailModal from './components/MailModal'
 
 function App() {
   //const [count, setCount] = useState(0)
-
+  const images = [
+    {
+      src: "public/Ten10.jpg",
+      description:
+        "Ten10 Academy 38. Alongside this group great professionals and people I began my journey into the tech industry.",
+    },
+    {
+      src: "public/donnell_mercedes_f1.jpg",
+      description:
+        "This is me! A trip to Battersea Power Station where I came into contact with a Lewis Hamilton x Mercedes championship winning car.",
+    },
+  ];
   return (
-
     <>
       <div>
         <header>
-          <h1>
-            Donnell Ofori
-          </h1>
+          <h1>Donnell Ofori</h1>
         </header>
-      <section className='about-me'> 
+        <section className="about-me">
           <p className="intro">
-            Hi there! I'm Donnell, a computer scientist dedicated to creating 
-            innovative solutions and pushing the boundaries of technology.
+            Hi there! I'm Donnell, a computer scientist dedicated to creating
+            innovative solutions and pushing the boundaries of technology. I
+            have skills in a range of areas in development and I am also a ISTQB
+            certified Software Tester.
           </p>
-          <p className='detail'>
-            I am also an author who has a deep desire to see new worlds and characters
-            come to life. Whether they mirror our own world, or create enitrely fantasitcal ones.
+          <p className="detail">
+            I am also an author who has a deep desire to see new worlds and
+            characters come to life. Whether they mirror our own world, or
+            create enitrely fantasitcal ones.
           </p>
         </section>
-        <div style={{padding:'5px', display:'flex', justifyContent:'center'}}>
-           <ContactButtons/>
+        <div
+          style={{ padding: "5px", display: "flex", justifyContent: "center" }}
+        >
+          <ContactButtons />
         </div>
-        {/* <MailModal/> */}
-       
-
+        <HomeCarousel images={images} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
--- src/components/ContactButtons.tsx
+++ src/components/ContactButtons.tsx
@@ -1,33 +1,31 @@
-import { MailIcon} from "lucide-react"
-import './ContactButtons.css'
+import { MailIcon } from "lucide-react";
+import "./ContactButtons.css";
 
-function ContactButtons () {
-    
-    const linkedIn = async () => {
-        window.open("https://www.linkedin.com/in/donnell-ofori-632546272/")   
-    }
+function ContactButtons() {
+  const linkedIn = async () => {
+    window.open("https://www.linkedin.com/in/donnell-ofori-632546272/");
+  };
 
-    const github = async () => {
-        window.open("https://github.com/donnell-10")
-    }
+  const github = async () => {
+    window.open("https://github.com/donnell-10");
+  };
 
-    
-    return(
-        <div className="buttons">
-            <button className="btn" onClick={linkedIn}>
-                <img src="/linkedin.svg" alt="linkedin" className="w-8 h-8"/>
-            </button>
-            <button className="btn" onClick={github}>
-                <img src="/github.svg" alt="github " className="w-8 h-8"/>
-                {/* <a target="_blank" href="https://icons8.com/icon/62856/github">GitHub</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
-            </button>               
-            <button className="btn">
-              <MailIcon className=""/>
-            </button>
-            
-                
-        </div>
-    )
+  return (
+    <div className="buttons">
+      <button className="btn" onClick={linkedIn}>
+        <img src="/linkedin.svg" alt="linkedin" className="w-8 h-8" />
+      </button>
+      <button className="btn" onClick={github}>
+        <img src="/github.svg" alt="github " className="w-8 h-8" />
+        {/* <a target="_blank" href="https://icons8.com/icon/62856/github">GitHub</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
+      </button>
+      <button className="btn">
+        <a href="mailto: oforidonnell@gmail.com">
+          <MailIcon style={{ color: "black" }} />
+        </a>
+      </button>
+    </div>
+  );
 }
 
 export default ContactButtons;
